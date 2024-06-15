6a025fd55e4ec884ae5782bef6617a7f9b893e30
LongVy2k2 <huahonglongvy2k2@gmail.com>
2024-06-15 18:10:57 +0700

    update footer 15/06

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,6 @@ function Footer() {
                 <div className={style.gradientText}>GOAT FITNESS</div>
             </div>
             <div className={style.footerItem}>
-                <div className={style.logo}>
                     <p className={style.addressText}>Địa chỉ của chúng tôi:</p>
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4937.465911635214!2d106.68677815437955!3d10.82130316398706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174deb3ef536f31%3A0x8b7bb8b7c956157b!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2hp4buHcCBUUC5IQ00!5e0!3m2!1svi!2s!4v1709737113792!5m2!1svi!2s"
@@ -18,36 +17,37 @@ function Footer() {
                         loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"
                     ></iframe>
-                </div>
             </div>
             <div className={style.footerItem}>
                 <div className={style.contact}>
                     <h1>Bạn cần hỗ trợ?</h1>
-                    <p>Liên hệ ngay:</p>
-                    <h2>0328 058 832</h2>
-                    <h2>Email: Nguyenlocface@gmail.com</h2>
+                    <h2>Liên hệ ngay:</h2>
+                    <p>SĐT 1: 0328 05 88 32</p>
+                    <p>SĐT 2: 0888 55 23 24</p>
+                    <p>Email 1: Nguyenlocface@gmail.com</p>
+                    <p>Email 2: Huahonglongvy2k2@gmail.com</p>
                 </div>
             </div>
             <div className={style.footerItem}>
                 <div className={style.explore}>
-                    <h1>Thông tin</h1>
-                    <a href="">Nội quy phòng tập</a>
+                    <h1 className={style.giandong}>Thông tin</h1>
+                    <a href="#">Nội quy phòng tập</a>
                     <hr />
-                    <a href="">Điều khoản</a>
+                    <a href="#">Điều khoản</a>
                     <hr />
-                    <a href="">Góp ý & phản hồi</a>
+                    <a href="#">Góp ý & phản hồi</a>
                     <hr />
-                    <a href="">Tuyển dụng</a>
+                    <a href="#">Tuyển dụng</a>
                 </div>
             </div>
             <div className={style.newSection1}>
                 <p>Copyright ©2024 All Rights Reserved by GOAT FITNESS</p>
                 <div className={style.socialMedia}>
-                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                    <a href="#" target="_blank" rel="noopener noreferrer">
                         <FaFacebook className={style.icon} />
                         Facebook
                     </a>
-                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                    <a href="#" target="_blank" rel="noopener noreferrer">
                         <FaInstagram className={style.icon} />
                         Instagram
                     </a>
